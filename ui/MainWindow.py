b84f023f92436cf6b728f9dfd0c9d3f195cea00a
209450 <209450@edu.p.lodz.pl>
2021-01-18 09:36:12 +0100

    added client table model

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QMainWindow
 
 from ui.WidgetFromFile import WidgetFromFile
@@ -9,3 +10,15 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.config_parser = WidgetFromFile.load_config_parser()
         WidgetFromFile.load_ui(self, self.config_parser["MainWindow"]['ui_path'])
+
+        self.client_table_model = QStandardItemModel()
+        self.init_client_table_view()
+
+    def init_client_table_view(self):
+        labels = self.config_parser["MainWindow"]["client_view_headers"].split()
+        self.client_table_model.setHorizontalHeaderLabels(labels)
+        self.clientTableView.setModel(self.client_table_model)
+
+        item = QStandardItem("aaa")
+        item1 = QStandardItem("bbb")
+        self.client_table_model.appendRow([item, item1])
